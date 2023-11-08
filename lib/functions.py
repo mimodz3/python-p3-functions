af2af3b62a32f54384c7aac20e7f1c5c0a74395a
mimodz3 <your.monysid@gmail.com>
2023-11-08 11:48:25 -0500

    py-functions

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,28 @@
 #!/usr/bin/env python3
 
-def greet_programmer():
+def greet_programmer():  
+    print("Hello, programmer!")
+ 
     pass
-
 def greet(name):
+    greeting = f"Hello, {name}!"  # Use an f-string to insert the 'name' variable into the greeting message
+    print(greeting)  # Print the greeting message
+
     pass
 
 def greet_with_default(name="programmer"):
+    print(f"Hello, {name}!")
+  
     pass
 
 def add(num1, num2):
+    return num1 + num2
     pass
 
 def halve(number):
+    if not isinstance(number, (int, float)):
+         return None
+    return number / 2
+         
+         
     pass
